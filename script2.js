f736e208b3f695b2ea1fa58d273a0d06204b628b
Williams <barroswilliams2001@gmail.com>
2021-11-14 19:03:44 -0300

    V3.0

---
+++ script2.js
@@ -0,0 +1,31 @@
+popupWhatsApp = () => {
+  
+  let btnClosePopup = document.querySelector('.closePopup');
+  let btnOpenPopup = document.querySelector('.whatsapp-button');
+  let popup = document.querySelector('.popup-whatsapp');
+  let sendBtn = document.getElementById('send-btn');
+
+  btnClosePopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+  })
+  
+  btnOpenPopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+     popup.style.animation = "fadeIn .10s 0.0s both";
+  })
+  
+  sendBtn.addEventListener("click", () => {
+  let msg = document.getElementById('whats-in').value;
+  let relmsg = msg.replace(/ /g,"%20");
+     
+   window.open('https://wa.me/+59898782940?text='+relmsg, '_blank'); 
+  
+  });
+
+  /*setTimeout(() => {
+    popup.classList.toggle('is-active-whatsapp-popup');
+  }, 0000);
+  */
+}
+
+popupWhatsApp();
